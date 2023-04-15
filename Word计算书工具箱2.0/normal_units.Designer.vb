5246f227abd1bb8ead1229194e42a7f50dbd0219
相君 阎 <xmascreater@163.com>
2023-04-15 20:15:53 +0800

    添加项目文件。

---
+++ Word计算书工具箱2.0/normal_units.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class normal_units
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(normal_units))
+        Me.Lst1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Lst1
+        '
+        Me.Lst1.Font = New System.Drawing.Font("Calibri", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lst1.FormattingEnabled = True
+        Me.Lst1.ItemHeight = 22
+        Me.Lst1.Location = New System.Drawing.Point(36, 14)
+        Me.Lst1.Name = "Lst1"
+        Me.Lst1.Size = New System.Drawing.Size(167, 114)
+        Me.Lst1.TabIndex = 21
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(82, 143)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 30)
+        Me.Button1.TabIndex = 22
+        Me.Button1.Text = "确定"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'normal_units
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(238, 184)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Lst1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "normal_units"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "单位推断"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Lst1 As Windows.Forms.ListBox
+    Friend WithEvents Button1 As Windows.Forms.Button
+End Class
